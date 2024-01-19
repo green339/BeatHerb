5930270a1a3af70b36f33b3cc60207b0ae9174bc
김도현 <nanayagoon@daum.net>
2024-01-19 14:59:14 +0900

    :tada: Init: Start Project

---
+++ backend/src/test/java/store/beatherb/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package store.beatherb.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
