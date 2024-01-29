b4c6d89ca2824a5814c5311b8509adfa23a1842c
김도현 <nanayagoon@daum.net>
2024-01-29 16:25:27 +0900

    test: DM 발송 및 저장 테스트

---
+++ backend/src/test/java/store/beatherb/restapi/directmessage/service/DirectMessageServiceImplTest.java
@@ -0,0 +1,52 @@
+package store.beatherb.restapi.directmessage.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import store.beatherb.restapi.member.domain.Member;
+import store.beatherb.restapi.member.domain.MemberRepository;
+import store.beatherb.restapi.member.dto.MemberDTO;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DirectMessageServiceImplTest {
+
+
+    @Autowired
+    DirectMessageService directMessageService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Test
+    void Kafka로메시지가_전송이_되야한다() throws Exception{
+
+        Member sender = Member.builder()
+                .picture("apple")
+                .nickname("감자자자")
+                .email("햄버ㅁㄴㅇ")
+                .name("고기반찬")
+                .advertise(false)
+                .build();
+
+        Member receiver = Member.builder()
+                .picture("apple")
+                .nickname("감자")
+                .email("햄버거")
+                .name("밥줘")
+                .advertise(false)
+                .build();
+        memberRepository.save(sender);
+        memberRepository.save(receiver);
+
+        assertNotNull(sender.getId());
+        assertNotNull(receiver.getId());
+
+        MemberDTO senderDTO = MemberDTO.toDTO(sender);
+        MemberDTO receiverDTO = MemberDTO.toDTO(receiver);
+
+        assertEquals(senderDTO.getId(),sender.getId());
+        assertEquals(receiverDTO.getId(),receiver.getId());
+        directMessageService.sendDirectMessage(senderDTO,receiverDTO,"헝그리정신이필요함");
+
+    }
+}
