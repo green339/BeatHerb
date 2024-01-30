f0cf928c5c04fc007bd6d6d3fb8eb05d2bdd5144
박도연 <ehduszkdzkd@naver.com>
2024-01-30 14:42:22 +0900

    Feat: 댓글 구현

--- backend/src/main/java/store/beatherb/restapi/content/exception/ContentErrorCode.java
+++ backend/src/main/java/store/beatherb/restapi/content/exception/ContentErrorCode.java
@@ -7,7 +7,10 @@ import store.beatherb.restapi.global.exception.ErrorCode;
 @Getter
 @AllArgsConstructor
 public enum ContentErrorCode implements ErrorCode {
-    LIST_HAS_NOT_CONTENT(404, "CONT_01", "리스트에 컨텐트가 담겨 있지 않습니다.");
+    CONTENT_ERROR_CODE(400, "CONT_01", "컨텐츠가 존재하지 않습니다."),
+    LIST_HAS_NOT_CONTENT(404, "CONT_02", "리스트에 컨텐트가 담겨 있지 않습니다."),
+    LIST_HAS_NOT_COMMENT(404, "CONT_03", "리스트에 코멘트가 담겨 있지 않습니다.");
+
 
     private final int statusCode;
     private final String errorCode;
