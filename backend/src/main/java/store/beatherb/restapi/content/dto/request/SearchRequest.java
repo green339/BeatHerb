04449133b291e842960af338ca35b2a436ef3885
최지희 <cjh0313c@gmail.com>
2024-01-30 23:21:52 +0900

    feat: querydsl 환경 수정

--- backend/src/main/java/store/beatherb/restapi/content/dto/request/SearchRequest.java
+++ backend/src/main/java/store/beatherb/restapi/content/dto/request/SearchRequest.java
@@ -4,12 +4,19 @@ import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
-@Builder
-@AllArgsConstructor
 public class SearchRequest {
-    String genre;
-    String bpm;
-    String keyNote;
-    String instrument;
+    String[] genre;
+    String[] bpm;
+    String[] keyNote;
+    String[] instrument;
     String keyword;
+
+    @Builder
+    public SearchRequest(String genre, String bpm, String keyNote, String instrument, String keyword) {
+        this.genre = genre.split(" ");
+        this.bpm = bpm.split(" ");
+        this.keyNote = keyNote.split(" ");
+        this.instrument = instrument.split(" ");
+        this.keyword = keyword;
+    }
 }
--- backend/src/main/java/store/beatherb/restapi/content/service/ContentSearchService.java
+++ backend/src/main/java/store/beatherb/restapi/content/service/ContentSearchService.java
@@ -1,12 +1,16 @@
 package store.beatherb.restapi.content.service;
 
+import com.querydsl.core.QueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import store.beatherb.restapi.content.dto.request.SearchRequest;
 import store.beatherb.restapi.content.dto.response.SearchResultResponse;
+import store.beatherb.restapi.member.domain.Member;
+import store.beatherb.restapi.member.domain.QMember;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -14,12 +18,7 @@ import java.util.Arrays;
 public class ContentSearchService {
 
     public SearchResultResponse search(SearchRequest searchRequest) {
-        String[] genre=searchRequest.getGenre().split(" ");
-        String[] bpm=searchRequest.getBpm().split(" ");
-        String[] keyNote=searchRequest.getKeyNote().split(" ");
-        String[] instrument=searchRequest.getInstrument().split(" ");
-        String keyword=searchRequest.getKeyword();
-        log.info(Arrays.toString(genre));
+        log.info(Arrays.toString(searchRequest.getGenre()));
         /*
         TODO: 쿼리 검색 로직 w/QueryDSL
          */
