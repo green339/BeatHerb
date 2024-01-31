462755af68077ec6ed6c9fdb2cc58042cc4d666a
최지희 <cjh0313c@gmail.com>
2024-02-01 08:24:23 +0900

    feat: querydsl 환경 수정

--- backend/src/main/java/store/beatherb/restapi/content/domain/ContentRepository.java
+++ backend/src/main/java/store/beatherb/restapi/content/domain/ContentRepository.java
@@ -1,5 +1,9 @@
 package store.beatherb.restapi.content.domain;
 
-public class ContentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContentRepository extends JpaRepository<Content,Long> {
 
 }
--- backend/src/main/java/store/beatherb/restapi/content/service/ContentSearchService.java
+++ backend/src/main/java/store/beatherb/restapi/content/service/ContentSearchService.java
@@ -1,28 +1,41 @@
 package store.beatherb.restapi.content.service;
 
-import com.querydsl.core.QueryFactory;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import store.beatherb.restapi.content.domain.QContent;
 import store.beatherb.restapi.content.dto.request.SearchRequest;
 import store.beatherb.restapi.content.dto.response.SearchResultResponse;
-import store.beatherb.restapi.member.domain.Member;
-import store.beatherb.restapi.member.domain.QMember;
+
 
 import java.util.Arrays;
 import java.util.List;
+
+import static store.beatherb.restapi.content.domain.QContent.content;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ContentSearchService {
-
+    private final EntityManager em;
     public SearchResultResponse search(SearchRequest searchRequest) {
         log.info(Arrays.toString(searchRequest.getGenre()));
         /*
         TODO: 쿼리 검색 로직 w/QueryDSL
          */
 
+//        JPAQueryFactory jpaQueryFactory=new JPAQueryFactory(em);
+//        jpaQueryFactory.select(content)
+//                .distinct()
+//                .from(content)
+//                .join()
+//                .join()
+//                .where(
+//
+//                )
+//                .fetch();
         return null;
     }
 }
