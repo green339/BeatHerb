cb179f1bd91cdac1d5cf2fd06156d1d6ffcb747f
김범수 <phsyobs@gmail.com>
2024-01-31 16:21:00 +0900

    Feat: DM 모달 상호작용 추가

--- frontend/src/page/MyPage.js
+++ frontend/src/page/MyPage.js
@@ -2,9 +2,10 @@
 
 import NavBar from "../components/NavBar";
 import ContentsItem from "../components/ContentsItem";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import ShortsItem from "../components/ShortsItem";
 import LiveItem from "../components/LiveItem";
+import Dm from "../components/Dm";
 
 // 탭 리스트
 const tabs = [
@@ -17,6 +18,7 @@ const tabs = [
 
 export default function MyPage() {
   const [category, setCategory] = useState("melody");
+  const dmModalRef = useRef();
   const tempArray = Array(20).fill().map((v,i)=>i+1);
 
   let itemList = null;
@@ -42,68 +44,78 @@ export default function MyPage() {
   }
 
   return (
-    <div className="h-full">
-      <div className="fixed top-0 w-full z-10">
-        <NavBar />
-      </div>
-      <div className="pt-[64px] mx-8">
-        <div className="mx-4 flex place-content-between">
-          <div className="flex gap-4">
-            <div className="flex place-items-center">
-              <div className="w-32 h-32 rounded-md">
-                <img className="w-full rounded-md" src="https://img.freepik.com/free-vector/background-colorful-musical-notes_23-2147633120.jpg?w=740&t=st=1705448093~exp=1705448693~hmac=00f2208917eeabe7c5309cb7efc90defc713277bede12138776ae696c5456d04" alt=""/>
+    <>
+      <div className="h-full">
+        <div className="fixed top-0 w-full z-10">
+          <NavBar />
+        </div>
+        <div className="pt-[64px] mx-8">
+          <div className="mx-4 flex place-content-between">
+            <div className="flex gap-4">
+              <div className="flex place-items-center">
+                <div className="w-32 h-32 rounded-md">
+                  <img className="w-full rounded-md" src="https://img.freepik.com/free-vector/background-colorful-musical-notes_23-2147633120.jpg?w=740&t=st=1705448093~exp=1705448693~hmac=00f2208917eeabe7c5309cb7efc90defc713277bede12138776ae696c5456d04" alt=""/>
+                </div>
+              </div>
+              <div>
+                <h1 className="text-base-content text-left">BeatHerb</h1>
+                <p className="text-base-content text-left">총 좋아요 수: 123456789</p>
+                <div className="flex gap-1 flex-wrap">
+                  <div className="badge badge-lg badge-primary text-primary-content">primary</div>
+                  <div className="badge badge-lg badge-primary text-primary-content">primary</div>
+                  <div className="badge badge-lg badge-primary text-primary-content">primary</div>
+                  <div className="badge badge-lg badge-primary text-primary-content">primary</div>
+                  <div className="badge badge-lg badge-primary text-primary-content">primary</div>
+                </div>
               </div>
             </div>
-            <div>
-              <h1 className="text-base-content text-left">BeatHerb</h1>
-              <p className="text-base-content text-left">총 좋아요 수: 123456789</p>
-              <div className="flex gap-1 flex-wrap">
-                <div className="badge badge-lg badge-primary text-primary-content">primary</div>
-                <div className="badge badge-lg badge-primary text-primary-content">primary</div>
-                <div className="badge badge-lg badge-primary text-primary-content">primary</div>
-                <div className="badge badge-lg badge-primary text-primary-content">primary</div>
-                <div className="badge badge-lg badge-primary text-primary-content">primary</div>
+            <div className="flex gap-4">
+              <div className="stats shadow">
+                <div className="stat place-items-center">
+                  <div className="stat-title">팔로잉</div>
+                  <div className="stat-value">998'244'353</div>
+                </div>
+                <div className="stat place-items-center">
+                  <div className="stat-title">팔로워</div>
+                  <div className="stat-value text-secondary">1,000,000,007</div>
+                </div>
+              </div>
+              <div className="flex place-items-center">
+                <button className="btn btn-ghost" onClick={() => dmModalRef.current?.showModal()}>
+                  <svg className="fill-primary" xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32">
+                    <path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/>
+                  </svg>
+                </button>
               </div>
             </div>
           </div>
-          <div className="flex gap-4">
-            <div className="stats shadow">
-              <div className="stat place-items-center">
-                <div className="stat-title">팔로잉</div>
-                <div className="stat-value">998'244'353</div>
-              </div>
-              <div className="stat place-items-center">
-                <div className="stat-title">팔로워</div>
-                <div className="stat-value text-secondary">1,000,000,007</div>
-              </div>
-            </div>
-            <div className="flex place-items-center">
-              <button className="btn btn-ghost">
-                <svg className="fill-primary" xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32">
-                  <path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/>
-                </svg>
-              </button>
-            </div>
+          <div role="tablist" className="tabs tabs-bordered my-8 tabs-lg">
+            {
+              tabs.map((tab) => (
+                <button
+                  key={tab.value}
+                  role="tab"
+                  className={"tab w-1/2 translate-x-1/2" + (category === tab.value ? " tab-active" : "")}
+                  onClick={() => setCategory(tab.value)}
+                >
+                  { tab.title }
+                </button>
+              ))
+            }
+          </div>
+          <div className="grid grid-cols-4 gap-4 items-center scrollbar scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-base-200 hover:scrollbar-thumb-primary overflow-y-scroll">
+            { itemList }
           </div>
         </div>
-        <div role="tablist" className="tabs tabs-bordered my-8 tabs-lg">
-          {
-            tabs.map((tab) => (
-              <button
-                key={tab.value}
-                role="tab"
-                className={"tab w-1/2 translate-x-1/2" + (category === tab.value ? " tab-active" : "")}
-                onClick={() => setCategory(tab.value)}
-              >
-                { tab.title }
-              </button>
-            ))
-          }
-        </div>
-        <div className="grid grid-cols-4 gap-4 items-center scrollbar scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-base-200 hover:scrollbar-thumb-primary overflow-y-scroll">
-          { itemList }
-        </div>
       </div>
-    </div>
+      <dialog className="modal" ref={dmModalRef}>
+        <div className="modal-box w-11/12 max-w-5xl bg-base-200">
+          <Dm />
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
   );
 }
--- frontend/src/page/Test.js
+++ frontend/src/page/Test.js
@@ -1,23 +1,22 @@
 // 컴포넌트가 제대로 만들어 졌는지 테스트하기 위한 공간
 
-import ContentsItem from '../components/ContentsItem.js';
-import LiveItem from '../components/LiveItem.js';
-import NavBar from '../components/NavBar.js';
-import ShortsItem from '../components/ShortsItem.js';
-import Board from './Board.js';
+import { useRef } from 'react';
+import Dm from '../components/Dm.js';
 
 export default function Test() {
+  const ref = useRef();
+
   return (
     <>
-      <NavBar />
-      <Board>
-        <p className="text-base-content">Page Content</p>
-      </Board>
-      <ContentsItem size={150} />
-      <ContentsItem size={200} />
-      <ContentsItem size={230} />
-      <ShortsItem />
-      <LiveItem />
+      <button className="btn" onClick={()=>ref.current?.showModal()}>open modal</button>
+      <dialog className="modal" ref={ref}>
+        <div className="modal-box w-11/12 max-w-5xl bg-base-200">
+          <Dm />
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </>
   );
 }
