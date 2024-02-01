cb7939f54dc897f0a2300eb85a809c4d8603e145
김도현 <nanayagoon@daum.net>
2024-02-01 20:45:42 +0900

    fix: 실시간 스트리밍 EndPoint 수정

--- backend/src/main/java/store/beatherb/restapi/content/controller/ContentController.java
+++ backend/src/main/java/store/beatherb/restapi/content/controller/ContentController.java
@@ -2,17 +2,24 @@ package store.beatherb.restapi.content.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import store.beatherb.restapi.content.domain.Content;
 import store.beatherb.restapi.content.service.ContentService;
 
+import java.io.File;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/content")
+@RequestMapping("/content")
 @Slf4j
 @RequiredArgsConstructor
 public class ContentController {
@@ -22,4 +29,23 @@ public class ContentController {
     public ResponseEntity<List<Content>> contentsOrderByHit(){
         return ResponseEntity.ok(contentService.getContentsOrderByHit());
     }
+
+    @GetMapping("/play/{contentNumber}")
+    public ResponseEntity<Resource> getPlayListByContentNumber(@PathVariable Integer contentNumber) {
+        Resource resource = contentService.getPlayList(contentNumber);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment;");
+        headers.setContentType(MediaType.parseMediaType("application/vnd.apple.mpegurl"));
+        return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/play/{contentNumber}/{segmentName:.+}")
+    public ResponseEntity<Resource> getSegment(@PathVariable Integer contentNumber,@PathVariable String segmentName) {
+
+        Resource resource = contentService.getSegmentByContentNumberAndSegmentName(contentNumber,segmentName);
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+    }
+
 }
--- backend/src/main/java/store/beatherb/restapi/content/service/ContentService.java
+++ backend/src/main/java/store/beatherb/restapi/content/service/ContentService.java
@@ -2,26 +2,60 @@ package store.beatherb.restapi.content.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import store.beatherb.restapi.content.domain.Content;
 import store.beatherb.restapi.content.domain.ContentRepository;
 import store.beatherb.restapi.content.exception.ContentErrorCode;
 import store.beatherb.restapi.content.exception.ContentException;
 
+import java.io.File;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @Slf4j
-@RequiredArgsConstructor
 public class ContentService {
+
+
+
     private final ContentRepository contentRepository;
+    private final String CROPPED_DIRECTORY;
+
+    public ContentService(ContentRepository contentRepository, @Value("${resource.directory.music.cropped}")String CROPPED_DIRECTORY) {
+        this.contentRepository = contentRepository;
+        this.CROPPED_DIRECTORY = CROPPED_DIRECTORY;
+    }
+
 
     public List<Content> getContentsOrderByHit(){
         Optional<List<Content>> contentListOptional = contentRepository.findAllByOrderByHitDesc();
-        List<Content> contentList = contentListOptional.orElseThrow(() -> new ContentException(ContentErrorCode.LIST_HAS_NOT_CONTENT));
+        return contentListOptional.orElseThrow(() -> new ContentException(ContentErrorCode.LIST_HAS_NOT_CONTENT));
+    }
 
+    public Resource getPlayList(Integer contentNumber){
+        String fileFullPath = CROPPED_DIRECTORY +
+                "/" +
+                contentNumber +
+                "/playlist.m3u8";
 
-        return contentList;
+        Resource resource = new FileSystemResource(fileFullPath);
+        if(resource.exists()){
+            return resource;
+        }
+        throw new ContentException(ContentErrorCode.CONTENT_NOT_FOUND);
+    }
+
+    public Resource getSegmentByContentNumberAndSegmentName(Integer contentNumber, String segmentName){
+        String fileFullPath = CROPPED_DIRECTORY + "/"+contentNumber+"/" + segmentName;
+        Resource resource = new FileSystemResource(fileFullPath);
+        if (resource.exists()) {
+            return resource;
+        }
+        throw new ContentException(ContentErrorCode.CONTENT_NOT_FOUND);
     }
 }
