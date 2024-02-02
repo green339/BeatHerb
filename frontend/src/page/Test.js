7f73a9f984a42ceea3bfd3968824f2a2de4ed453
김도현 <nanayagoon@daum.net>
2024-02-02 15:41:26 +0900

    fix: conflict 해결

--- frontend/src/page/Test.js
+++ frontend/src/page/Test.js
@@ -1,24 +1,19 @@
 // 컴포넌트가 제대로 만들어 졌는지 테스트하기 위한 공간
 
-<<<<<<< HEAD
 import ChatYou from '../components/ChatYou'
 import ChatMe from '../components/ChatMe'
-=======
 import ContentsItem from '../components/ContentsItem.js';
 import LiveItem from '../components/LiveItem.js';
 import Live from '../components/Live.js';
 import NavBar from '../components/NavBar.js';
 import ShortsItem from '../components/ShortsItem.js';
 import Board from './Board.js';
->>>>>>> 4663c17736b815fc8345b7c25568cc1fd23ed8b1
 
 export default function Test() {
   return (
     <>
-<<<<<<< HEAD
       <ChatMe />
       <ChatYou />
-=======
       <NavBar />
       <Board>
         <p className="text-base-content">Page Content</p>
@@ -29,7 +24,6 @@ export default function Test() {
       <ShortsItem />
       <LiveItem />
       <Live />
->>>>>>> 4663c17736b815fc8345b7c25568cc1fd23ed8b1
     </>
   );
 }
