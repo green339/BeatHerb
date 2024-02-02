8476f16d9db7e05038ec5544fca6ae1d827224a3
박도연 <ehduszkdzkd@naver.com>
2024-02-02 14:35:24 +0900

    Feat: 채팅 리스트 모달 style->tailwind로 변경

--- frontend/src/components/ChatYou.js
+++ frontend/src/components/ChatYou.js
@@ -3,9 +3,9 @@
 export default function ChatYou(){
     return(
         <div>
-            <p  className="font-semibold text-cyan-950" style={{textAlign: 'left'}}>2024-01-06 16:33:11</p>
-            <div className="bg-primary" style={{width: '450px', height: '50px',borderRadius: '10px 10px 10px 0px'}}>
-                <p className="font-bold pl-6 pt-3 mt-2 text-cyan-950" style={{textAlign: 'left'}}>채팅이 들어올 예정</p>
+            <p  className="font-semibold text-cyan-950 text-left">2024-01-06 16:33:11</p>
+            <div className="bg-primary w-96 h-12 rounded-tl-lg rounded-tr-lg rounded-br-lg">
+                <p className="font-bold pl-6 pt-3 mt-2 text-cyan-950 text-left">채팅이 들어올 예정</p>
             </div>
         </div>
     );
