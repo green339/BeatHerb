25ea1090debb1253e8160e98e4c53a5119550608
최지희 <cjh0313c@gmail.com>
2024-02-05 08:36:48 +0900

    feat: 편집기능

--- frontend/src/page/WorkPlace.js
+++ frontend/src/page/WorkPlace.js
@@ -1,5 +1,3 @@
 export default function WorkPlace() {
-  return (
-    <p className="text-primary text-3xl font-semibold">작업실 화면이에요~~~~~~~~</p>
-  );
+  return <p className="text-primary text-3xl font-semibold">작업실 화면이에요~~~~~~~~</p>;
 }
