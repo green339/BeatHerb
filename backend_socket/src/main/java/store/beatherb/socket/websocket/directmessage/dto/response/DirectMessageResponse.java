4be2fa1def8aa2431c7d047f2a6aa0d8d02f8c3a
김도현 <nanayagoon@daum.net>
2024-02-06 15:15:04 +0900

    fix: DM 소켓 수신 오류 수정

--- backend_socket/src/main/java/store/beatherb/socket/websocket/directmessage/dto/response/DirectMessageResponse.java
+++ backend_socket/src/main/java/store/beatherb/socket/websocket/directmessage/dto/response/DirectMessageResponse.java
@@ -1,11 +1,16 @@
 package store.beatherb.socket.websocket.directmessage.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -18,7 +23,11 @@ public class DirectMessageResponse {
     DMMember sender;
     DMMember receiver;
 
-    Timestamp createdAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    LocalDateTime createdAt;
     String message;
     @Data
     public static class DMMember{
