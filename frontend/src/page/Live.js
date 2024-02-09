bd8fad2b22a226595535320d0049b684e3d1dbdc
김범수 <phsyobs@gmail.com>
2024-02-09 18:52:54 +0900

    Feat: 라이브 화면 틀에 라이브 기능 추가

--- frontend/src/page/Live.js
+++ frontend/src/page/Live.js
@@ -3,37 +3,48 @@ import { useEffect, useState, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { OpenVidu } from "openvidu-browser";
 import { useStateCallback } from "../hook/useStateCallback";
+import NavBar from "../components/NavBar";
 
-function OpenViduVideoComponent(props) {
+function OpenViduVideoComponent({ streamManager, width, height }) {
   const videoRef = useRef();
 
   useEffect(() => {
-    if (props && !!videoRef) {
-      props.streamManager.addVideoElement(videoRef.current);
+    if (streamManager && !!videoRef) {
+      streamManager.addVideoElement(videoRef.current);
     }
   }, [])
 
   useEffect(() => {
-    if (props && !!videoRef) {
-      props.streamManager.addVideoElement(videoRef.current);
+    if (streamManager && !!videoRef) {
+      streamManager.addVideoElement(videoRef.current);
     }
   })
 
-  return <video autoPlay={true} ref={videoRef} />;
+  return <video autoPlay={true} ref={videoRef} width height className="object-fill" />;
 }
 
-function UserVideoComponent(props) {
+function UserVideoComponent({ streamManager, width, height, mainVideo = false }) {
   function getNicknameTag() {
     // Gets the nickName of the user
-    return JSON.parse(props.streamManager.stream.connection.data).clientData;
+    return JSON.parse(streamManager.stream.connection.data).clientData;
   }
 
   return (
     <div>
-      {props.streamManager !== undefined ? (
-        <div className="streamcomponent">
-          <OpenViduVideoComponent streamManager={props.streamManager} />
-          <div><p>{getNicknameTag()}</p></div>
+      {streamManager !== undefined ? (
+        <div className="relative" style={{ width, height }}>
+          <OpenViduVideoComponent streamManager={streamManager} width height />
+          {mainVideo ? (
+            <div className="absolute top-[20px] left-[20px]">
+              <svg width="100" height="35" viewBox="0 0 100 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="100" height="35" rx="17.5" fill="#FF0000"/>
+                <path d="M23.5 23.3125C25.125 23.3125 26.5 22.75 27.625 21.625C28.75 20.5 29.3125 19.125 29.3125 17.5C29.3125 15.875 28.75 14.5 27.625 13.375C26.5 12.25 25.125 11.6875 23.5 11.6875C21.875 11.6875 20.5 12.25 19.375 13.375C18.25 14.5 17.6875 15.875 17.6875 17.5C17.6875 19.125 18.25 20.5 19.375 21.625C20.5 22.75 21.875 23.3125 23.5 23.3125ZM23.5 30C21.7917 30 20.1771 29.6719 18.6562 29.0156C17.1354 28.3594 15.8073 27.4635 14.6719 26.3281C13.5365 25.1927 12.6406 23.8646 11.9844 22.3438C11.3281 20.8229 11 19.2083 11 17.5C11 15.7708 11.3281 14.1458 11.9844 12.625C12.6406 11.1042 13.5365 9.78125 14.6719 8.65625C15.8073 7.53125 17.1354 6.64062 18.6562 5.98438C20.1771 5.32812 21.7917 5 23.5 5C25.2292 5 26.8542 5.32812 28.375 5.98438C29.8958 6.64062 31.2188 7.53125 32.3438 8.65625C33.4688 9.78125 34.3594 11.1042 35.0156 12.625C35.6719 14.1458 36 15.7708 36 17.5C36 19.2083 35.6719 20.8229 35.0156 22.3438C34.3594 23.8646 33.4688 25.1927 32.3438 26.3281C31.2188 27.4635 29.8958 28.3594 28.375 29.0156C26.8542 29.6719 25.2292 30 23.5 30ZM23.5 28.125C26.4583 28.125 28.9688 27.0885 31.0312 25.0156C33.0938 22.9427 34.125 20.4375 34.125 17.5C34.125 14.5417 33.0938 12.0312 31.0312 9.96875C28.9688 7.90625 26.4583 6.875 23.5 6.875C20.5625 6.875 18.0573 7.90625 15.9844 9.96875C13.9115 12.0312 12.875 14.5417 12.875 17.5C12.875 20.4375 13.9115 22.9427 15.9844 25.0156C18.0573 27.0885 20.5625 28.125 23.5 28.125Z" fill="white"/>
+                <path d="M49.4091 23V11.3636H50.8182V21.75H56.2273V23H49.4091ZM59.8182 11.3636V23H58.4091V11.3636H59.8182ZM63.1207 11.3636L66.5753 21.1591H66.7116L70.1662 11.3636H71.6435L67.3707 23H65.9162L61.6435 11.3636H63.1207ZM73.456 23V11.3636H80.4787V12.6136H74.8651V16.5455H80.1151V17.7955H74.8651V21.75H80.5696V23H73.456Z" fill="white"/>
+              </svg>
+            </div>) : null}
+          <div className="absolute bg-white left-0 bottom-0 px-2">
+            <p className="text-lg text-black">{getNicknameTag()}</p>
+          </div>
         </div>
       ) : null}
     </div>
@@ -41,10 +52,6 @@ function UserVideoComponent(props) {
 }
 
 export default function Live() {
-  //몇 명의 호스트가 라이브에 참여하는가
-  //ex) 대표 호스트 한명, 서브 호스트 두명
-  const [hostNum, setHostNum] = useState(3);
-
   const location = useLocation();
   const token = location.state?.token || undefined;
   const role = location.state?.role || undefined;
@@ -54,7 +61,7 @@ export default function Live() {
   // OpenVidu 관련 상태 값
   const ov = useRef(null); 
   const [mySessionId, setMySessionId] = useState('SessionA');
-  const [myUserName, setMyUserName] = useState('Participant' + Math.floor(Math.random() * 100));
+  const [myUserName, setMyUserName] = useState("호스트");
   const [session, setSessionCallback] = useStateCallback();
   const [mainStreamManager, setMainStreamManager] = useState();  // Main video of the page. Will be the 'publisher' or one of the 'subscribers'
   const [publisher, setPublisher] = useState();
@@ -190,126 +197,46 @@ export default function Live() {
   }, []);
 
   return (
-    <div className="container w-full h-full">
-      {session === undefined ? (
-        <div id="join">
-          <div id="img-div">
-            <img src="resources/images/openvidu_grey_bg_transp_cropped.png" alt="OpenVidu logo" />
+    <>
+      <div className="fixed top-0 w-full z-10">
+        <NavBar />
+      </div>
+      <div className="pt-16 grid grid-cols-10">
+        <div className="col-span-7">
+          <div className="mt-6 mb-4 flex justify-center items-center">
+            {mainStreamManager !== undefined ? (
+              <UserVideoComponent streamManager={mainStreamManager} width={640} height={480} mainVideo />
+            ) : null}
           </div>
-          <div id="join-dialog" className="jumbotron vertical-center">
-            <h1> Join a video session </h1>
-            <form className="form-group" onSubmit={joinSession}>
-              <p>
-                <label>Participant: </label>
-                <input
-                  className="form-control"
-                  type="text"
-                  id="userName"
-                  value={myUserName}
-                  onChange={handleChangeUserName}
-                  required
-                />
-              </p>
-              <p>
-                <label> Session: </label>
-                <input
-                  className="form-control"
-                  type="text"
-                  id="sessionId"
-                  value={mySessionId}
-                  onChange={handleChangeSessionId}
-                  required
-                />
-              </p>
-              <p className="text-center">
-                <input className="btn btn-lg btn-success" name="commit" type="submit" value="JOIN" />
-              </p>
-            </form>
-          </div>
-        </div>
-      ) : null}
-
-      {session !== undefined ? (
-        <div id="session">
-          <div id="session-header">
-            <h1 id="session-title">{mySessionId}</h1>
-            <input
-              className="btn btn-large btn-danger"
-              type="button"
-              id="buttonLeaveSession"
-              onClick={leaveSession}
-              value="Leave session"
-            />
-            {/* <input
-              className="btn btn-large btn-success"
-              type="button"
-              id="buttonSwitchCamera"
-              onClick={switchCamera}
-              value="Switch Camera"
-            /> */}
-          </div>
-
-          {mainStreamManager !== undefined ? (
-            <div id="main-video" className="col-md-6">
-              <UserVideoComponent streamManager={mainStreamManager} />
+          {publisher !== undefined ? (
+            <div key="main" className="inline-block mr-4 relavive">
+              <UserVideoComponent streamManager={mainStreamManager} width={160} height={120} />
             </div>
           ) : null}
-          <div id="video-container" className="col-md-6">
-            {publisher !== undefined ? (
-              <div className="stream-container col-md-6 col-xs-6" onClick={() => handleMainVideoStream(publisher)}>
-                <UserVideoComponent streamManager={publisher} />
-              </div>
-            ) : null}
-            {subscribers.map((sub, i) => (
-              <div key={sub.id} className="stream-container col-md-6 col-xs-6" onClick={() => handleMainVideoStream(sub)}>
-                <span>{sub.id}</span>
-                <UserVideoComponent streamManager={sub} />
-              </div>
-            ))}
+          {publisher !== undefined ? (
+            <div key="main2" className="inline-block mr-4 relavive">
+              <UserVideoComponent streamManager={mainStreamManager} width={160} height={120} />
+            </div>
+          ) : null}
+          {subscribers.map((sub, i) => (
+            <div key={"sub"+i} className="inline-block mr-4 relavive">
+              <UserVideoComponent streamManager={mainStreamManager} width={160} height={120} />
+            </div>
+          ))}
+          <div style={{ paddingLeft: '180px' }}>
+            <p className="text-white font-bold text-2xl text-left">Google이 심심해서 하는 라이브</p>
+            <p className="text-white text-left">@구글 자작곡, @구글 자작 곡곡, @애국가</p>
           </div>
         </div>
-      ) : null}
-    </div>
-  )
-  // return (
-  //   <div className="grid grid-cols-10">
-  //     <div className="col-span-7">
-  //       <div className="mt-6 mb-4 flex justify-center items-center">
-  //         <div style={{position: 'relative'}}>
-  //           <img src="https://img.freepik.com/free-photo/medium-shot-man-wearing-vr-glasses_23-2150394443.jpg?w=1060&t=st=1706683154~exp=1706683754~hmac=3ea592dfa518b96373d1613b8dde42a208dd565b677cd813f2d6f021e1991531" alt="live" width="700" height="400"/>
-  //           <div style={{position: 'absolute', top: '20px', left: '20px'}}>
-  //             <svg xmlns="http://www.w3.org/2000/svg" width="150" height="53" fill="none"><rect width="150" height="52.5" fill="red" rx="26.25"/><path fill="#fff" d="M74.864 35V17.546h2.113v15.579h8.114V35H74.864Zm15.613-17.454V35h-2.113V17.546h2.113Zm4.954 0 5.182 14.693h.204l5.182-14.693h2.216L101.806 35h-2.182l-6.409-17.454h2.216ZM110.934 35V17.546h10.534v1.875h-8.42v5.897h7.875v1.875h-7.875v5.932h8.556V35h-10.67Z"/><path fill="#fff" d="M36.75 35.719c2.438 0 4.5-.844 6.188-2.532 1.687-1.687 2.53-3.75 2.53-6.187 0-2.438-.843-4.5-2.53-6.188-1.688-1.687-3.75-2.53-6.188-2.53-2.438 0-4.5.843-6.188 2.53-1.687 1.688-2.53 3.75-2.53 6.188 0 2.438.843 4.5 2.53 6.188 1.688 1.687 3.75 2.53 6.188 2.53Zm0 10.031c-2.563 0-4.984-.492-7.266-1.477-2.28-.984-4.273-2.328-5.976-4.03-1.703-1.704-3.047-3.696-4.031-5.977C18.492 31.984 18 29.563 18 27c0-2.594.492-5.031 1.477-7.313.984-2.28 2.328-4.265 4.03-5.953 1.704-1.687 3.696-3.023 5.977-4.007 2.282-.985 4.703-1.477 7.266-1.477 2.594 0 5.031.492 7.313 1.477 2.28.984 4.265 2.32 5.953 4.007 1.687 1.688 3.023 3.672 4.007 5.954.985 2.28 1.477 4.718 1.477 7.312 0 2.563-.492 4.984-1.477 7.266-.984 2.28-2.32 4.273-4.007 5.976-1.688 1.703-3.672 3.047-5.953 4.031-2.282.985-4.72 1.477-7.313 1.477Zm0-2.813c4.438 0 8.203-1.554 11.297-4.664 3.094-3.109 4.64-6.867 4.64-11.273 0-4.438-1.546-8.203-4.64-11.297-3.094-3.094-6.86-4.64-11.297-4.64-4.406 0-8.164 1.546-11.273 4.64-3.11 3.094-4.665 6.86-4.665 11.297 0 4.406 1.555 8.164 4.665 11.273 3.109 3.11 6.867 4.664 11.273 4.664Z"/></svg>
-  //           </div>
-  //           <div style={{position: 'absolute', width: '80px', height: '30px', backgroundColor: 'white', left: '0', bottom: '0'}}>
-  //             <p style={{fontSize: '20px'}}>호스트1</p>
-  //           </div>
-  //         </div>
-  //       </div>
-  //       {
-  //         Array(hostNum-1).fill().map((v, index) => {
-  //           return (
-  //             <div key={index} className="inline-block mr-4" style={{position: 'relative'}}>
-  //               <img src="https://img.freepik.com/free-psd/live-streaming-3d-render-icon_47987-9046.jpg?w=900&t=st=1706676747~exp=1706677347~hmac=95d8db0ac477ba72d79177fd6767d0ac55f92c56a131381c4b983363d998ab33" alt="live" width="200" height="200"/>
-  //               <div style={{position: 'absolute', width: '80px', height: '30px', backgroundColor: 'white', left: '0', bottom: '0'}}>
-  //                 <p style={{fontSize: '20px'}}>호스트{index+2}</p>
-  //               </div>
-  //             </div>
-  //           )
-  //         })
-  //       }
-  //       <div style={{ paddingLeft: '180px' }}>
-  //         <p className="text-white font-bold text-2xl text-left">Google이 심심해서 하는 라이브</p>
-  //         <p className="text-white text-left">@구글 자작곡, @구글 자작 곡곡, @애국가</p>
-  //       </div>
-  //     </div>
-  //     <div className="col-span-3 flex justify-center items-center">
-  //       <div style={{ width: '400px', height: '680px', backgroundColor: 'white', position: 'relative'}} className="items-start">
-  //         <div style={{ width: '250px', height: '80px', position: 'absolute', right: '120px', bottom: '30px'}} className="bg-gray-200"/>
-  //         <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48" style={{ position: 'absolute', right: '40px', bottom: '50px'}}>
-  //           <path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/>
-  //         </svg>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+        <div className="col-span-3 flex justify-center items-center">
+          <div style={{ width: '400px', height: '680px', backgroundColor: 'white', position: 'relative'}} className="items-start">
+            <div style={{ width: '250px', height: '80px', position: 'absolute', right: '120px', bottom: '30px'}} className="bg-gray-200"/>
+            <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48" style={{ position: 'absolute', right: '40px', bottom: '50px'}}>
+              <path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
