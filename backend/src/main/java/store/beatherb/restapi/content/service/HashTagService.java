a7571a0605bf87532886ef855c7d2b34adcb7e4e
김도현 <nanayagoon@daum.net>
2024-02-11 23:11:01 +0900

    feat: 해쉬태그 기준 사용하는 컨텐츠/멤버 조회

--- backend/src/main/java/store/beatherb/restapi/content/service/HashTagService.java
+++ backend/src/main/java/store/beatherb/restapi/content/service/HashTagService.java
@@ -3,6 +3,8 @@ package store.beatherb.restapi.content.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import store.beatherb.restapi.content.domain.ContentHashTag;
+import store.beatherb.restapi.content.domain.ContentHashTagRepository;
 import store.beatherb.restapi.content.domain.HashTag;
 import store.beatherb.restapi.content.domain.HashTagRepository;
 import store.beatherb.restapi.content.dto.request.DeleteHashTagRequest;
@@ -10,10 +12,13 @@ import store.beatherb.restapi.content.dto.request.HashTagSearchNameRequest;
 import store.beatherb.restapi.content.dto.request.RegistHashTagRequest;
 import store.beatherb.restapi.content.dto.request.UpdateHashTagRequest;
 import store.beatherb.restapi.content.dto.response.HashTagListResponse;
+import store.beatherb.restapi.content.dto.response.HashTagUseResponse;
 import store.beatherb.restapi.content.dto.response.RegistHashTagResponse;
 import store.beatherb.restapi.content.dto.response.UpdateHashTagResponse;
 import store.beatherb.restapi.content.exception.HashTagErrorCode;
 import store.beatherb.restapi.content.exception.HashTagException;
+import store.beatherb.restapi.interest.domain.Interest;
+import store.beatherb.restapi.interest.domain.InterestRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +28,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class HashTagService {
     private final HashTagRepository hashTagRepository;
+    private final ContentHashTagRepository contentHashTagRepository;
+    private final InterestRepository interestRepository;
 
     public RegistHashTagResponse registHashTag(RegistHashTagRequest registHashTagRequest){
         HashTag hashTag = HashTag.builder()
@@ -73,5 +80,18 @@ public class HashTagService {
             result.add(HashTagListResponse.toDto(t));
         }
         return result;
+    }
+
+    public HashTagUseResponse findMemberAndContentByUsingHashTagId(Long id) {
+        HashTag hashTag = hashTagRepository.findById(id).orElseThrow(
+                () -> {
+                    return new HashTagException(HashTagErrorCode.HASHTAG_IS_NOT_EXIST);
+                }
+        );
+        List<ContentHashTag> contentHashTagList = contentHashTagRepository.findByHashTag(hashTag);
+        //여기서 list 로 가져올것임.
+        List<Interest> interestList  = interestRepository.findByHashTag(hashTag);
+
+        return HashTagUseResponse.toDto(interestList,contentHashTagList);
     }
 }
