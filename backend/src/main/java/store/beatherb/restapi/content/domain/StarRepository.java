fb161ad844bb7eca71a5aac9611f6664c44626e7
김도현 <nanayagoon@daum.net>
2024-02-13 13:28:43 +0900

    fix: 반환값 수정

--- backend/src/main/java/store/beatherb/restapi/content/domain/StarRepository.java
+++ backend/src/main/java/store/beatherb/restapi/content/domain/StarRepository.java
@@ -2,10 +2,12 @@ package store.beatherb.restapi.content.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import store.beatherb.restapi.member.domain.Member;
 
 import java.util.Optional;
 
 @Repository
-public interface FavoriteRepository extends JpaRepository<Star, Long> {
+public interface StarRepository extends JpaRepository<Star, Long> {
     Optional<Star> findByContentIdAndMemberId(Long contentId, Long memberId);
+    Optional<Star> findByContentAndMember(Content content, Member member);
 }
--- backend/src/main/java/store/beatherb/restapi/content/exception/FavoriteErrorCode.java
+++ backend/src/main/java/store/beatherb/restapi/content/exception/FavoriteErrorCode.java
@@ -8,7 +8,8 @@ import store.beatherb.restapi.global.exception.ErrorCode;
 @AllArgsConstructor
 public enum FavoriteErrorCode implements ErrorCode {
 
-    FAVORITE_FIND_ERROR(404, "FAV_01", "좋아요가 존재하지 않습니다.");
+    FAVORITE_FIND_ERROR(404, "FAV_01", "좋아요가 존재하지 않습니다."),
+    ALREADY_EXISTS(400, "FAV_02" , "이미 좋아요 하셨습니다.");
 
     private final int statusCode;
     private final String errorCode;
