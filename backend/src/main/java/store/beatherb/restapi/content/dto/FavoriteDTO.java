bf02a00bb32eeeebe40821a43761b1e317a763d2
유시연 <nova0109@naver.com>
2024-02-13 13:06:10 +0900

    fix: Service 수정 및 Controller 추가

--- backend/src/main/java/store/beatherb/restapi/content/dto/FavoriteDTO.java
+++ backend/src/main/java/store/beatherb/restapi/content/dto/FavoriteDTO.java
@@ -6,19 +6,20 @@ import lombok.Data;
 import store.beatherb.restapi.content.domain.Content;
 import store.beatherb.restapi.content.domain.Star;
 import store.beatherb.restapi.member.domain.Member;
+import store.beatherb.restapi.member.dto.MemberDTO;
 
 @Data
 @AllArgsConstructor
 @Builder
 public class FavoriteDTO {
     private long id;
-    private Content content;
-    private Member member;
+    private ContentDTO content;
+    private MemberDTO member;
     public static FavoriteDTO toDTO(Star entity) {
         return FavoriteDTO.builder()
               .id(entity.getId())
-              .content(entity.getContent())
-              .member(entity.getMember())
+              .content(ContentDTO.toDto(entity.getContent()))
+              .member(MemberDTO.toDTO(entity.getMember()))
               .build();
     }
 }
